efd7ba6346c0aebadc1a18652e3fd41a3b8a3e83
rigicmilap <164999163+rigicmilap@users.noreply.github.com>
2024-08-30 19:20:41 +0530

    google map changes

--- src/jsx/pages/Subdivision/GoogleMapLocator.js
+++ src/jsx/pages/Subdivision/GoogleMapLocator.js
@@ -8,7 +8,7 @@ import { Form,Offcanvas } from "react-bootstrap";
 import Select from "react-select";
 import AdminSubdevisionService from "../../../API/Services/AdminService/AdminSubdevisionService";
 import Button from "react-bootstrap/Button";
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 
 const containerStyle = {
@@ -22,7 +22,7 @@ const defaultCenter = {
 };
 
 const GoogleMapLocator = () => {
-
+  const navigate = useNavigate();
   const location = useLocation();
   const state = location.state || {}; // Safely access state with fallback
   
@@ -33,6 +33,7 @@ const GoogleMapLocator = () => {
 
   const [builderList, setBuilderList] = useState([]);
   const subdivisionList = state.subdivisionList;
+console.log("subdivisionList",subdivisionList);
 
   const [selectedMarker, setSelectedMarker] = useState(null);
   // const [builderId, setBuilderId] = useState('');
@@ -165,6 +166,16 @@ const GoogleMapLocator = () => {
 
   return ( 
       <LoadScript googleMapsApiKey="AIzaSyDY5Dsqd_6ZlAWwdohre3Fiz3K8hbRqcAE">
+         <div style={{height: "40px"}}>
+            <Button
+               className="btn btn-primary btn-sm me-1"
+               style={{marginTop: "5px", marginLeft: "5px"}}
+               onClick={() => navigate('/subdivisionlist')}
+            >
+            <i className="fa fa-arrow-left" aria-hidden="true" style={{marginRight: "10px"}}></i>
+            Go Back
+            </Button>
+         </div>
         <GoogleMap mapContainerStyle={containerStyle} zoom={8} center={defaultCenter}
          options={{
             mapTypeId: "satellite",
@@ -186,280 +197,138 @@ const GoogleMapLocator = () => {
               onCloseClick={() => setSelectedMarker(null)}
             >
               <div>
-                <h4>{selectedMarker.name}</h4>
+                <h4>{selectedMarker.builder.name}<br/>{selectedMarker.name}</h4>
+                <hr style={{ borderWidth: "2px", borderStyle: "solid", borderColor: "black", marginTop: "0px" }} />
+
                 <div>
-                Reporting :<span>
-                {selectedMarker.reporting === 1 && "Yes"}
-                {selectedMarker.reporting === 0 && "No"}
+                Status : <span>
+                {selectedMarker.status === 1 && "Yes"}
+                {selectedMarker.status === 0 && "No"}
                 </span>
                 </div>
+
                 <div>
-                Product Type :<span>
+                Product Type : <span>
                      {selectedMarker.product_type}
                 </span>
                 </div>
 
                 <div>
-                Area :<span>
+                Area : <span>
                      {selectedMarker.area}
                 </span>
                 </div>
-            <div>
-                Masterplan :<span>
+
+               <div>
+                Masterplan : <span>
                      {selectedMarker.masterplan_id == ''? 'NA' :selectedMarker.masterplan_id}
                 </span>
-             </div>
-             <div>
-                Zip Code :<span>
-                     {selectedMarker.zipcode == ''? 'NA' :selectedMarker.zipcode}
+               </div>
+
+               <div>
+                Open Since : <span>
+                     {selectedMarker.opensince == ''? 'NA' :selectedMarker.opensince}
                 </span>
-             </div>
-             <div>
-                Total lots :<span>
+               </div>
+
+               <div>
+                Total lots : <span>
                      {selectedMarker.zipcode == ''? 'NA' :selectedMarker.totallots}
                 </span>
-             </div>
-             <div>
-                Total Width :<span>
-                     {selectedMarker.lotwidth == ''? 'NA' :selectedMarker.lotwidth}
+               </div>
+
+               <div>
+                  Unsold Lots : <span>{selectedMarker.unsold_lots} </span>
+               </div>
+
+               <div>
+                  Total Net Sales : <span>
+                     {selectedMarker.total_net_sales  == ''? 'NA' :selectedMarker.total_net_sales}
+                  </span>
+               </div>
+
+               <div>
+                  Avg Net Sales Per Month Since Open : <span>{selectedMarker.avg_net_sales_per_month_since_open || "NA"} </span>
+               </div>
+
+               <div style={{marginTop: "20px"}}>
+                  Lot Width : <span>{selectedMarker.lotwidth || "NA"} </span>
+               </div>
+
+               <div>
+                  Lot Size : <span>{selectedMarker.lotsize || "NA"} </span>
+               </div>
+
+               <div>
+                  All Single Story : <span>
+                     {selectedMarker.single === 1 && "Yes"}
+                     {selectedMarker.single === 0 && "No"}
+                  </span>
+               </div>
+
+               <div>
+                  Gated : <span>
+                     {selectedMarker.gated === 1 && "Yes"}
+                     {selectedMarker.gated === 0 && "No"}
+                  </span>
+               </div> 
+
+               <div>
+                  Age Restricted : <span>
+                     {selectedMarker.age === 1 && "Yes"}
+                     {selectedMarker.age === 0 && "No"}
+                  </span>
+               </div>
+
+               <div>
+                Juridiction : <span>
+                     {selectedMarker.juridiction == ''? 'NA' :selectedMarker.juridiction}
                 </span>
              </div>
+
              <div>
-                Total Size :<span>
-                     {selectedMarker.lotsize == ''? 'NA' :selectedMarker.lotsize}
-                </span>
-             </div>
-             <div>
-                Zoning :<span>
+                Zoning : <span>
                      {selectedMarker.zoning == ''? 'NA' :selectedMarker.zoning}
                 </span>
              </div>
+
              <div>
-                Age Restricted :<span>
-                {selectedMarker.age === 1 && "Yes"}
-                {selectedMarker.age === 0 && "No"}
-                </span>
-             </div>
-             <div>
-                All Single Story :<span>
-                {selectedMarker.single === 1 && "Yes"}
-                {selectedMarker.single === 0 && "No"}
-                </span>
-             </div>
-             <div>
-                Gated :<span>
-                {selectedMarker.gated === 1 && "Yes"}
-                {selectedMarker.gated === 0 && "No"}
-                </span>
-             </div> 
-             <div>
-                Juridiction :
-                <span>
-                     {selectedMarker.juridiction == ''? 'NA' :selectedMarker.juridiction}
-                </span>
-             </div> 
-             <div>
-                Longitude :
-                <span>
-                     {selectedMarker.lng == ''? 'NA' :selectedMarker.lng}
-                </span>
-             </div> 
-             <div>
-                Latitude :
-                <span>
-                     {selectedMarker.lat == ''? 'NA' :selectedMarker.lat}
-                </span>
-             </div> 
-             <div>
-                Gas Provider :
-                <span>
+                Gas Provider : <span>
                      {selectedMarker.gasprovider == ''? 'NA' :selectedMarker.gasprovider}
                 </span>
-             </div> 
-             <div>
-                HOA Fee :
-                <span>
-                     {selectedMarker.hoafee == ''? 'NA' :selectedMarker.hoafee}
-                </span>
-             </div> 
-             <div>
-                Masterplan Fee :
-                <span>
-                     {selectedMarker.masterplanfee == ''? 'NA' :selectedMarker.masterplanfee}
-                </span>
-             </div> 
-             <div>
-                Parcel Group :
-                <span>
-                     {selectedMarker.parcel == ''? 'NA' :selectedMarker.parcel}
-                </span>
-             </div> 
-             <div>
-                Phone :
-                <span>
-                     {selectedMarker.phone == ''? 'NA' :selectedMarker.phone}
-                </span>
              </div>
-             <div>
-                Website :
-                <span>
-                     {selectedMarker.builder.website == ''? 'NA' :selectedMarker.builder.website}
-                </span>
-             </div>  
-             <div>
-             _fkBuilderID :
-                <span>
-                     {selectedMarker.builder.builder_code  == ''? 'NA' :selectedMarker.builder.builder_code}
-                </span>
-             </div>
-             <div>
-                Date Added :
-                <span>
-                <DateComponent date={selectedMarker.created_at} />
-                </span>
-             </div> 
-             <div>
-             Total Closing :
-                <span>
-                     {selectedMarker.total_closings  == ''? 'NA' :selectedMarker.total_closings}
-                </span>
-             </div>
-             <div>
-             Total Permits :
-                <span>
+
+             <div style={{marginTop: "20px"}}>
+             Total Permits : <span>
                      {selectedMarker.total_permits  == ''? 'NA' :selectedMarker.total_permits}
                 </span>
              </div>
+
              <div>
-             Total Net Sales :
-                <span>
-                     {selectedMarker.total_net_sales  == ''? 'NA' :selectedMarker.total_net_sales}
+             Total Closing : <span>
+                     {selectedMarker.total_closings  == ''? 'NA' :selectedMarker.total_closings}
                 </span>
              </div>
+
              <div>
-             Months Open :
-                <span>
-                     {selectedMarker.months_open  == ''? 'NA' :selectedMarker.months_open}
-                </span>
+             Latest Lots Released : <span>{selectedMarker.latest_lots_released} </span>
              </div>
+
              <div>
-             Latest Traffic/Sales Data :
-               <DateComponent date={selectedMarker.latest_traffic_data} />
+             Latest Standing Inventory : <span>{selectedMarker.latest_standing_inventory} </span>
              </div>
+
              <div>
-             Latest Lots Released :
-            <span>{selectedMarker.latest_lots_released} </span>
+             Avg Sqft All : <span>{selectedMarker.avg_sqft_all} </span>
              </div>
+
              <div>
-             Latest Standing Inventory :
-               <span>{selectedMarker.latest_standing_inventory} </span>
-             </div>
-             <div>
-             Unsold Lots :
-               <span>{selectedMarker.unsold_lots} </span>
-             </div>
-             <div>
-             Avg Sqft All :
-               <span>{selectedMarker.avg_sqft_all} </span>
-             </div>
-             <div>
-             Avg Sqft Active :
-               <span>{selectedMarker.avg_sqft_active} </span>
-             </div>
-             <div>
-                Avg Base Price Active :
-                <span>{<PriceComponent price={selectedMarker.avg_base_price_all} /> || "NA"} </span>
-            </div>
-            <div>
-                Avg Base Price Active :
-                <span>{<PriceComponent price={selectedMarker.avg_base_price_active} /> || "NA"} </span>
-            </div>
-            <div>
-            Min Sqft All :
-                <span>{selectedMarker.min_sqft_all || "NA"} </span>
-            </div>
-            <div>
-            Min Sqft Active :
-                <span>{selectedMarker.min_sqft_active || "NA"} </span>
-            </div>
-            <div>
-            Max Sqft All :
-                <span>{selectedMarker.max_sqft_all || "NA"} </span>
-            </div>
-            <div>
-            Max Sqft Active :
-                <span>{selectedMarker.max_sqft_active || "NA"} </span>
-            </div>
-            <div>
-            Min Base Price All :
-            {<PriceComponent price={selectedMarker.min_base_price_all} /> || "NA"}
-            </div>
-            <div>
-            Min SQFT Active Current :
-            <span>{selectedMarker.min_sqft_active_current || "NA"} </span>
-            </div>
-            <div>
-            Max Base Price All :
-            {<PriceComponent price={selectedMarker.max_base_price_all} /> || "NA"}        
-            </div>
-            <div>
-            Min SQFT Active Current :
-            <span>{selectedMarker.max_sqft_active_current || "NA"} </span>
-            </div>
-            <div>
-            Min SQFT Active Current :
-            <span>{selectedMarker.max_sqft_active_current || "NA"} </span>
-            </div>
-            <div>
-            Avg Net Traffic Per Month This Year  :
-            <span>{selectedMarker.avg_net_traffic_per_month_this_year || "NA"} </span>
+                Avg Base Price All : <span>{<PriceComponent price={selectedMarker.avg_base_price_all} /> || "NA"} </span>
             </div>
 
             <div>
-            Avg Net Traffic Per Month This Year :
-            <span>{selectedMarker.avg_net_traffic_per_month_this_year || "NA"} </span>
+                Avg Closing Price : <span>{<PriceComponent price={selectedMarker.avg_closing_price} /> || "NA"} </span>
             </div>
-
-            <div>
-            Avg Net Sales Per Month This Year :
-            <span>{selectedMarker.avg_net_sales_per_month_this_year || "NA"} </span>
-            </div>
-
-            <div>
-            Avg Closings Per Month This Year :
-            <span>{selectedMarker.avg_closings_per_month_this_year || "NA"} </span>
-            </div>
-
-            <div>
-            Avg Net Sales Per Month Since Open :
-            <span>{selectedMarker.avg_net_sales_per_month_since_open || "NA"} </span>
-            </div>
-            
-            <div>
-            Avg Net Sales Per Month Last Three Months :
-            <span>{selectedMarker.avg_net_sales_per_month_last_three_months || "NA"} </span>
-            </div>
-
-            <div>
-            Avg Net Sales Per Month Last Three Months :
-            <span>{selectedMarker.avg_net_sales_per_month_last_three_months || "NA"} </span>
-            </div>
-
-            <div>
-            Avg Net Sales Per Month Last Three Months :
-            <span>{selectedMarker.avg_net_sales_per_month_last_three_months || "NA"} </span>
-            </div>
-            <div>
-            Max Week Ending :
-            {<DateComponent date={selectedMarker.max_week_ending} /> || "NA"}
-            </div>
-
-            <div>
-
-            Min Week Ending :
-        {<DateComponent date={selectedMarker.min_week_ending} /> || "NA"}
-        </div>
-
             </div>
             </InfoWindow>
           )}
