47e1ff21c585508b84843d153be32bb630a36708
rigicmilap <164999163+rigicmilap@users.noreply.github.com>
2024-11-20 12:21:35 +0530

    forgot password link set

--- src/jsx/pages/Login.js
+++ src/jsx/pages/Login.js
@@ -1,21 +1,25 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import { connect } from "react-redux";
-import { Link, NavLink, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import AdminUserService from "../../API/Services/AdminService/AdminUserService";
 import logo from "../../images/logo/hrb-logo.png";
 import LogoWhite from "../../images/logo/logofull-white.png";
 import bg6 from "../../images/background/bg6.jpg";
+import { Modal, Button, Form } from 'react-bootstrap';
 
 function Login(props) {
   const [heartActive, setHeartActive] = useState(true);
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
+  const [emailSend, setSendEmail] = useState("");
+  const [emailCorrect, setEmailCorrect] = useState(false);
   let errorsObj = { email: "", password: "" };
   const [errors, setErrors] = useState(errorsObj);
   const [password, setPassword] = useState("");
   const [rememberMe, setRememberMe] = useState(false);
   var d = new Date();
+  const [forgotModelShow, setForgotModelShow] = useState(false);
 
 
   const handleRememberMeChange = () => {
@@ -49,7 +53,7 @@ function Login(props) {
         if (user != "") {
           const userRole = JSON.parse(localStorage.getItem("user")).role;
           console.log(userRole);
-          if(!userRole) {
+          if (!userRole) {
             alert(user.message);
             return;
           }
@@ -284,127 +288,157 @@ function Login(props) {
       }
     }
   };
-  return (
-    <div className="page-wraper">
-      <div className="browse-job login-style3">
-        <div
-          className="bg-img-fix overflow-hidden"
-          style={{ background: "#fff url(" + bg6 + ")", height: "100vh" }}
-        >
-          <div className="row gx-0">
-            <div className="col-xl-4 col-lg-5 col-md-6 col-sm-12 vh-100 bg-white ">
-              <div
-                id="mCSB_1"
-                className="mCustomScrollBox mCS-light mCSB_vertical mCSB_inside"
-                style={{ maxHeight: "653px" }}
-              >
-                <div
-                  id="mCSB_1_container"
-                  className="mCSB_container"
-                  style={{
-                    position: "relative",
-                    top: "0",
-                    left: "0",
-                    dir: "ltr",
-                  }}
-                >
-                  <div className="login-form style-2">
-                    <div className="card-body">
-                      <div className="logo-header">
-                        <Link to={"#"} className="logo">
-                          <img
-                            src={logo}
-                            alt=""
-                            className="width-230 light-logo"
-                          />
-                        </Link>
-                        <Link to={"#"} className="logo">
-                          <img
-                            src={LogoWhite}
-                            alt=""
-                            className="width-230 dark-logo"
-                          />
-                        </Link>
-                      </div>
-                      <div className="nav nav-tabs border-bottom-0">
-                        <div className="tab-content w-100" id="nav-tabContent">
-                          <div
-                            className="tab-pane fade active show"
-                            id="nav-personal"
-                          >
-                            {props.errorMessage && (
-                              <div className="bg-red-300 text-red-900 border border-red-900 p-1 my-2">
-                                {props.errorMessage}
-                              </div>
-                            )}
-                            {props.successMessage && (
-                              <div className="bg-green-300 text-green-900 border border-green-900 p-1 my-2">
-                                {props.successMessage}
-                              </div>
-                            )}
-                            <form className=" dz-form pb-3" onSubmit={onLogin}>
-                              <h3 className="form-title m-t0">
-                                Personal Information
-                              </h3>
-                              <div className="dz-separator-outer m-b5">
-                                <div className="dz-separator bg-primary style-liner"></div>
-                              </div>
-                              <p>
-                                Enter your e-mail address and your password.{" "}
-                              </p>
-                              <p className="text-danger">{errors.message} </p>
 
-                              <div className="form-group mb-3">
-                                <input
-                                  type="email"
-                                  className="form-control"
-                                  value={email}
-                                  onChange={(e) => setEmail(e.target.value)}
-                                />
-                                {errors.email && (
-                                  <div className="text-danger fs-12">
-                                    {errors.email}
-                                  </div>
-                                )}
-                              </div>
-                              <div className="form-group mb-3">
-                                <input
-                                  type="password"
-                                  className="form-control"
-                                  value={password}
-                                  onChange={(e) => setPassword(e.target.value)}
-                                />
-                                {errors.password && (
-                                  <div className="text-danger fs-12">
-                                    {errors.password}
-                                  </div>
-                                )}
-                              </div>
-                              <div className="form-group text-left mb-5">
-                                <button
-                                  type="submit"
-                                  className="btn btn-primary dz-xs-flex m-r5"
-                                >
-                                  login
-                                </button>
-                                <span className="form-check d-inline-block ms-2">
+  const handlEmailClose = () => {
+    setForgotModelShow(false); 
+    setEmailCorrect(false);
+    setSendEmail("");
+  };
+
+  const handleSendEmail = () => {
+    // Validate email (for example, basic format check)
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!email || !emailRegex.test(email)) {
+      setEmailCorrect(true);
+      return;
+    }
+
+    // Call your backend API to send the reset password email
+    // Example:
+    // fetch('/api/send-reset-email', { method: 'POST', body: JSON.stringify({ email }) });
+
+    console.log('Sending email to:', email);
+    // Close modal after sending email (you can modify this based on your API response)
+    setForgotModelShow(false);
+  };
+
+  return (
+    <Fragment>
+      <div className="page-wraper">
+        <div className="browse-job login-style3">
+          <div
+            className="bg-img-fix overflow-hidden"
+            style={{ background: "#fff url(" + bg6 + ")", height: "100vh" }}
+          >
+            <div className="row gx-0">
+              <div className="col-xl-4 col-lg-5 col-md-6 col-sm-12 vh-100 bg-white ">
+                <div
+                  id="mCSB_1"
+                  className="mCustomScrollBox mCS-light mCSB_vertical mCSB_inside"
+                  style={{ maxHeight: "653px" }}
+                >
+                  <div
+                    id="mCSB_1_container"
+                    className="mCSB_container"
+                    style={{
+                      position: "relative",
+                      top: "0",
+                      left: "0",
+                      dir: "ltr",
+                    }}
+                  >
+                    <div className="login-form style-2">
+                      <div className="card-body">
+                        <div className="logo-header">
+                          <Link to={"#"} className="logo">
+                            <img
+                              src={logo}
+                              alt=""
+                              className="width-230 light-logo"
+                            />
+                          </Link>
+                          <Link to={"#"} className="logo">
+                            <img
+                              src={LogoWhite}
+                              alt=""
+                              className="width-230 dark-logo"
+                            />
+                          </Link>
+                        </div>
+                        <div className="nav nav-tabs border-bottom-0">
+                          <div className="tab-content w-100" id="nav-tabContent">
+                            <div
+                              className="tab-pane fade active show"
+                              id="nav-personal"
+                            >
+                              {props.errorMessage && (
+                                <div className="bg-red-300 text-red-900 border border-red-900 p-1 my-2">
+                                  {props.errorMessage}
+                                </div>
+                              )}
+                              {props.successMessage && (
+                                <div className="bg-green-300 text-green-900 border border-green-900 p-1 my-2">
+                                  {props.successMessage}
+                                </div>
+                              )}
+                              <form className=" dz-form pb-3" onSubmit={onLogin}>
+                                <h3 className="form-title m-t0">
+                                  Personal Information
+                                </h3>
+                                <div className="dz-separator-outer m-b5">
+                                  <div className="dz-separator bg-primary style-liner"></div>
+                                </div>
+                                <p>
+                                  Enter your e-mail address and your password.{" "}
+                                </p>
+                                <p className="text-danger">{errors.message} </p>
+
+                                <div className="form-group mb-3">
                                   <input
-                                    type="checkbox"
-                                    className="form-check-input"
-                                    id="check1"
-                                    name="example1"
-                                    checked={rememberMe}
-                                    onChange={handleRememberMeChange}
+                                    type="email"
+                                    className="form-control"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
                                   />
-                                  <label
-                                    className="form-check-label"
-                                    htmlFor="check1"
+                                  {errors.email && (
+                                    <div className="text-danger fs-12">
+                                      {errors.email}
+                                    </div>
+                                  )}
+                                </div>
+                                <div className="form-group mb-3">
+                                  <input
+                                    type="password"
+                                    className="form-control"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                  />
+                                  {errors.password && (
+                                    <div className="text-danger fs-12">
+                                      {errors.password}
+                                    </div>
+                                  )}
+                                </div>
+                                <div className="form-group text-left mb-5">
+                                  <button
+                                    type="submit"
+                                    className="btn btn-primary dz-xs-flex m-r5"
                                   >
-                                    Remember me
-                                  </label>
-                                </span>
-                              </div>
-                              {/* <div className="dz-social">
+                                    login
+                                  </button>
+                                  <span className="form-check d-inline-block ms-2">
+                                    <input
+                                      type="checkbox"
+                                      className="form-check-input"
+                                      id="check1"
+                                      name="example1"
+                                      checked={rememberMe}
+                                      onChange={handleRememberMeChange}
+                                    />
+                                    <label
+                                      className="form-check-label"
+                                      htmlFor="check1"
+                                    >
+                                      Remember me
+                                    </label>
+                                  </span>
+                                  <div className="form-group mb-3 text-right" style={{ marginTop: "10px" }}>
+                                    <Link className="text-primary" onClick={() => setForgotModelShow(true)}>
+                                      Forgot Password?
+                                    </Link>
+                                  </div>
+                                </div>
+                                {/* <div className="dz-social">
 																<h5 className="form-title fs-20">Sign In With</h5>
 																<ul className="dz-social-icon dz-border dz-social-icon-lg text-white">
 																	<li><a target="_blank" href="https://www.facebook.com/dexignzone" className="fab fa-facebook-f btn-facebook"></a></li>
@@ -413,31 +447,32 @@ function Login(props) {
 																	<li><a target="_blank" href="https://twitter.com/dexignzones" className="fab fa-twitter btn-twitter"></a></li>
 																</ul>
 															</div> */}
-                            </form>
-                            {/* <div className="text-center bottom">
+                              </form>
+                              {/* <div className="text-center bottom">
 															<NavLink to="/page-register" className="btn btn-primary button-md btn-block" >
 																Create an account
 															</NavLink>
 														</div> */}
+                            </div>
                           </div>
                         </div>
                       </div>
-                    </div>
-                    <div className="card-footer">
-                      <div className=" bottom-footer clearfix m-t10 m-b20 row text-center">
-                        <div className="col-lg-12 text-center">
-                          <p>
-                            {" "}
-                            © 2024 Website Developed by{" "}
-                            <a
-                              href="https://www.rigicglobalsolutions.com/"
-                              target="_blank"
-                              rel="noreferrer"
-                            >
-                              Rigic Global Solutions Pvt Ltd
-                            </a>{" "}
-                            All Rights Reserved
-                          </p>{" "}
+                      <div className="card-footer">
+                        <div className=" bottom-footer clearfix m-t10 m-b20 row text-center">
+                          <div className="col-lg-12 text-center">
+                            <p>
+                              {" "}
+                              © 2024 Website Developed by{" "}
+                              <a
+                                href="https://www.rigicglobalsolutions.com/"
+                                target="_blank"
+                                rel="noreferrer"
+                              >
+                                Rigic Global Solutions Pvt Ltd
+                              </a>{" "}
+                              All Rights Reserved
+                            </p>{" "}
+                          </div>
                         </div>
                       </div>
                     </div>
@@ -448,7 +483,47 @@ function Login(props) {
           </div>
         </div>
       </div>
-    </div>
+
+      {/* Reset Password */}
+      <Modal show={forgotModelShow} onHide={() => setForgotModelShow(false)}>
+        <>
+          <Modal.Header>
+            <Modal.Title>Forgot Password</Modal.Title>
+            <button
+              className="btn-close"
+              aria-label="Close"
+              onClick={() => handlEmailClose()}
+            ></button>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group controlId="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter your email"
+                  value={emailSend}
+                  onChange={(e) => setSendEmail(e.target.value)} // Set email state
+                />
+                {emailCorrect && (
+                  <div style={{ color: 'red', marginTop: '5px' }}>
+                    Please enter a valid email address
+                  </div>
+                )}
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={() => handlEmailClose()}>
+              Cancel
+            </Button>
+            <Button variant="primary" onClick={handleSendEmail}>
+              Send
+            </Button>
+          </Modal.Footer>
+        </>
+      </Modal>
+    </Fragment>
   );
 }
 
