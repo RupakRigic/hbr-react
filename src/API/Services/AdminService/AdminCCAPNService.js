367087442133f9608d2c0c02013f1049efeb70db
rigicmilap <164999163+rigicmilap@users.noreply.github.com>
2024-12-20 13:06:15 +0530

    ccapns changes

--- src/API/Services/AdminService/AdminCCAPNService.js
+++ src/API/Services/AdminService/AdminCCAPNService.js
@@ -1,7 +1,7 @@
 import clientAuth from "../../clientAuth";
 
 export default {
-    index: (page=1,searchQuery = '',sortConfig ='') => clientAuth.get(`admin/ccapn/index?page=${page}${searchQuery}${sortConfig}`),
+    index: (page=1) => clientAuth.get(`admin/ccapn/index?page=${page}`),
     manageAccessFields:(userData) =>  clientAuth.post(`admin/ccapn/manage-access-fields`,{ json: userData }),
     bulkupdate: (id, userData) => clientAuth.put(`admin/ccapn/bulkupdate/${id}`, { json: userData }),
 };
